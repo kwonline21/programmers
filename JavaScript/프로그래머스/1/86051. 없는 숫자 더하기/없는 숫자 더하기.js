a08db8f99c4b49b63e483dc10956c6701acbd3b7
kwonline21 <kwonline21@gmail.com>
2024-09-03 09:18:12 +0900

    Update 없는 숫자 더하기.js

--- JavaScript/프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ JavaScript/프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,4 +1,12 @@
-function solution(numbers) {
-    var answer = -1;
-    return answer;
-}
+const solution = (numbers) => {
+  const set = new Set(numbers);
+  let answer = 0;
+  for (let i = 0; i < 10; i++) {
+    if (set.has(i)) {
+      continue;
+    } else {
+      answer += i;
+    }
+  }
+  return answer;
+};
