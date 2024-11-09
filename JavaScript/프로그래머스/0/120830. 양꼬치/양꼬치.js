96464fd66f33485aa1b8fe814f9419eb868fe2fb
kwonline21 <kwonline21@gmail.com>
2024-11-09 14:01:22 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120830. 양꼬치/양꼬치.js
+++ JavaScript/프로그래머스/0/120830. 양꼬치/양꼬치.js
@@ -1,10 +1,4 @@
 const solution = (n, k) => {
-  let service = 0;
-  let payment = n * 12000;
-  while (n > 9) {
-    n -= 10;
-    service++;
-  }
-  payment += (k - service) * 2000;
+  let payment = n * 12000 + (k - Math.floor(n / 10)) * 2000;
   return payment;
 };
